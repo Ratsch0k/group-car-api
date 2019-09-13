5ef222aade5c3b53f844626c429f7cada5cdc6b7
Simon Kurz <simonlukas.98@gmail.com>
2019-09-13 23:55:26 +0200

    added travis file, move main script to server.js, added deploy script
    Added .travis.yml:
    for integration with Travis CI
    
    Moved main script:
    Main script previously located in /bin/www move topmost level and renamed to server.js, easier access for service file
    changed outdated use of var to const or let
    
    Remove content of public folder:
    Content in public folder not needed because server only intended as api server, should not distribute normal html resources like html or pictures
    
    Added deploy.sh script in new folder _scripts:
    Used by Travis CI to deploy to server
    Prepares service files and pushes to server if conditions are met. For now encryption of ssh key and generally ssh key is missing for server.
    => Not working for now. Missing ssh key (encrypted)

---
+++ _scripts/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -x
+
+if [ $TRAVIS_BRANCH = "master" ]; then
+
+    # encrypt key
+    # missing line, encrypts key
+    rm deploy-key.enc
+    chmod 600 deploy-key
+    mv deploy-key ~/.ssh/id_rsa
+
+    # Create service file
+    SERVICE_CONTENT = "[Unit]\n"
+    SERVICE_CONTENT = "${SERVICE_CONTENT}Description=Api server for group-car. Handles api requests, not used as static resource distribution\n\n"
+    SERVICE_CONTENT = "${SERVICE_CONTENT}[Service]\n"
+    SERVICE_CONTENT = "${SERVICE_CONTENT}ExecStart=$SERVER_PATH/server.js\n"
+    SERVICE_CONTENT = "${SERVICE_CONTENT}User:$SERVER_USER\n"
+    SERVICE_CONTENT = "${SERVICE_CONTENT}Group:$SERVER_GROUP"
+
+    touch server.service
+    echo $SERVICE_CONTENT > server.service
+
+    # Create local repository and push to server
+    git init
+
+    git remote add deploy "$SERVER_USER@$SERVER_IP}:$SERVER_PATH}"
+    git config.username = "Travis CI"
+
+    git add .
+    git commit -m "Deploy Commit: $TRAVIS_COMMIT"
+    git push --force deploy master
+
+else
+    echo "Not on master branch, not deploying"
+fi
