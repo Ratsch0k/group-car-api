1b2424725ba840d8693495035da7c3425c7b49dd
Simon Kurz <simonlukas.98@gmail.com>
2019-09-14 23:53:22 +0200

    Debugging not working service start

--- _scripts/ci-dep.sh
+++ _scripts/ci-dep.sh
@@ -18,7 +18,7 @@ if [ $TRAVIS_BRANCH = "master" ]; then
     SERVICE_CONTENT=$"${SERVICE_CONTENT}Description=Api server for group-car. Handles api requests, not used as static resource distribution\n"
     SERVICE_CONTENT=$"${SERVICE_CONTENT}After=network.target\n\n"
     SERVICE_CONTENT=$"${SERVICE_CONTENT}[Service]\n"
-    SERVICE_CONTENT=$"${SERVICE_CONTENT}Type=forking\n"
+    #SERVICE_CONTENT=$"${SERVICE_CONTENT}Type=forking\n"
     SERVICE_CONTENT=$"${SERVICE_CONTENT}ExecStart=$SERVER_PATH/server/server.js\n"
     SERVICE_CONTENT=$"${SERVICE_CONTENT}WorkingDirectory=$SERVER_PATH/server\n"
     SERVICE_CONTENT=$"${SERVICE_CONTENT}Environment=PATH=/usr/bin:/usr/local/bin\n"
