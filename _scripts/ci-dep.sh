2c6369c35df9a7a488ff5e12458a106ec4585d2c
Simon Kurz <simonlukas.98@gmail.com>
2019-09-14 22:18:00 +0200

    Updated deploy script: new lines should be created inside service file

--- _scripts/ci-dep.sh
+++ _scripts/ci-dep.sh
@@ -14,12 +14,12 @@ if [ $TRAVIS_BRANCH = "master" ]; then
     mv deploy-key ~/.ssh/id_rsa
 
     # Create service file
-    SERVICE_CONTENT="[Unit]\n"
-    SERVICE_CONTENT="${SERVICE_CONTENT}Description=Api server for group-car. Handles api requests, not used as static resource distribution\n\n"
-    SERVICE_CONTENT="${SERVICE_CONTENT}[Service]\n"
-    SERVICE_CONTENT="${SERVICE_CONTENT}ExecStart=$SERVER_PATH/server.js\n"
-    SERVICE_CONTENT="${SERVICE_CONTENT}User:$SERVER_USER\n"
-    SERVICE_CONTENT="${SERVICE_CONTENT}Group:$SERVER_GROUP"
+    SERVICE_CONTENT=$"[Unit]\n"
+    SERVICE_CONTENT=$"${SERVICE_CONTENT}Description=Api server for group-car. Handles api requests, not used as static resource distribution\n\n"
+    SERVICE_CONTENT=$"${SERVICE_CONTENT}[Service]\n"
+    SERVICE_CONTENT=$"${SERVICE_CONTENT}ExecStart=$SERVER_PATH/server.js\n"
+    SERVICE_CONTENT=$"${SERVICE_CONTENT}User:$SERVER_USER\n"
+    SERVICE_CONTENT=$"${SERVICE_CONTENT}Group:$SERVER_GROUP"
 
     touch server.service
     echo "$SERVICE_CONTENT" > server.service
