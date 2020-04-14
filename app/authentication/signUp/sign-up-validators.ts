74f715a674d5436975a951b069ad98a1cc8d0d02
Simon Kurz <simonlukas.98@gmail.com>
2020-04-14 20:50:18 +0200

    Added sanitizing email to signup validator

--- app/authentication/signUp/sign-up-validators.ts
+++ app/authentication/signUp/sign-up-validators.ts
@@ -2,8 +2,8 @@ import {check} from 'express-validator';
 
 export default {
   validator: [
-    check('username').notEmpty(),
-    check('email').isEmail()
+    check('username').notEmpty().escape().trim(),
+    check('email').escape().trim().isEmail()
         .withMessage('Email has to be a valid email address'),
     check('password').isLength({min: 6})
         .withMessage('Password has to be at least 6 characters long'),
