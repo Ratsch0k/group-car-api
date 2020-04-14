25c8ec6c1ed3203294625e481dbf663e34fdcf11
Simon Kurz <simonlukas.98@gmail.com>
2020-04-14 15:58:40 +0200

    Moved logic of sign up router to controller
    Router file now only creates the router and nothing else

--- app/authentication/signUp/signUpController.ts
+++ app/authentication/signUp/signUpController.ts
@@ -1,4 +1,12 @@
+import debug from 'debug';
+import {validationResult} from 'express-validator';
+import InvalidRequestError from '@app/errors/invalidRequestError';
+
 type RequestHandler = import('express').RequestHandler;
+
+const log = debug('group-car:signup:controller:log');
+const error = debug('group-car:signup:controller:error');
+
 /**
  * Signs the user up.\
  * Creates a new user with the given properties.
@@ -7,7 +15,16 @@ type RequestHandler = import('express').RequestHandler;
  * @param password  Password
  */
 const signUpController: RequestHandler = (req, res) => {
-  throw new Error('NO');
+  log('IP %s requested sign up', req.ip);
+
+  // Check for validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    error('Sign up for IP %s failed validation', req.ip);
+    throw new InvalidRequestError(errors);
+  } else {
+    log('Sign up for IP %s passed validation', req.ip);
+  }
 };
 
 export default signUpController;
--- app/authentication/signUp/signUpRouter.ts
+++ app/authentication/signUp/signUpRouter.ts
@@ -1,29 +1,12 @@
-import express = require('express');
-import debug = require('debug');
+import * as express from 'express';
 import signUpValidators from '@app/authentication/signUp/signUpValidators';
 import signUpController from '@app/authentication/signUp/signUpController';
-import {validationResult} from 'express-validator';
-import InvalidRequestError from '@app/errors/invalidRequestError';
-debug('group-car:login');
-const router: express.Router = express.Router();
 
-/**
- * Sign up router
- * @param req Http request
- * @param res Http response
- */
-const signUpRouter: express.RequestHandler = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    throw new InvalidRequestError(errors);
-  } else {
-    next();
-  }
-};
+const router: express.Router = express.Router();
 
 /**
  * Add the {@link signUpRouter} to the router
  */
-router.put('/', signUpValidators.validator, signUpRouter, signUpController);
+router.put('/', signUpValidators.validator, signUpController);
 
 export default router;
