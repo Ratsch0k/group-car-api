b4b39b78c8becb419efa6633871c587eb16958b9
Simon Kurz <simonlukas.98@gmail.com>
2020-04-14 15:58:05 +0200

    Added messages to sign up validators

--- app/authentication/signUp/signUpValidators.ts
+++ app/authentication/signUp/signUpValidators.ts
@@ -3,7 +3,9 @@ import {check} from 'express-validator';
 export default {
   validator: [
     check('username').notEmpty(),
-    check('email').isEmail(),
-    check('password').isLength({min: 6, max: Number.POSITIVE_INFINITY}),
+    check('email').isEmail()
+        .withMessage('Email has to be a valid email address'),
+    check('password').isLength({min: 6})
+        .withMessage('Password has to be at least 6 characters long'),
   ],
 };
