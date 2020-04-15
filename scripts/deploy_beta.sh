33e9601e4221495c598199ac0cc6dcfc476759fc
Simon Kurz <simonlukas.98@gmail.com>
2020-04-15 21:59:06 +0200

    Added new stages to pipeline, added new scripts

--- scripts/deploy_beta.sh
+++ scripts/deploy_beta.sh
@@ -13,19 +13,19 @@ mv deploy-key ~/.ssh/id_rsa
  
 # Create service file
 SERVICE_CONTENT=$"[Unit]\n"
-SERVICE_CONTENT=$"${SERVICE_CONTENT}Description=Api server for group-car. Handles api requests and serves frontend\n"
+SERVICE_CONTENT=$"${SERVICE_CONTENT}Description=Beta api server for group-car. Handles api requests and serves frontend. For not completely released features.\n"
 SERVICE_CONTENT=$"${SERVICE_CONTENT}After=network.target\n\n"
 SERVICE_CONTENT=$"${SERVICE_CONTENT}[Service]\n"
-SERVICE_CONTENT=$"${SERVICE_CONTENT}ExecStart=/usr/bin/node -r tsconfig-paths/register $SERVER_PATH/build/group-car.js\n"
-SERVICE_CONTENT=$"${SERVICE_CONTENT}WorkingDirectory=$SERVER_PATH\n"
+SERVICE_CONTENT=$"${SERVICE_CONTENT}ExecStart=/usr/bin/node -r tsconfig-paths/register $SERVER_PATH_BETA/build/group-car.js\n"
+SERVICE_CONTENT=$"${SERVICE_CONTENT}WorkingDirectory=$SERVER_PATH_BETA\n"
 SERVICE_CONTENT=$"${SERVICE_CONTENT}Environment=PATH=/usr/bin:/usr/local/bin\n"
 SERVICE_CONTENT=$"${SERVICE_CONTENT}Environment=DEBUG=group-car:*\n"
 SERVICE_CONTENT=$"${SERVICE_CONTENT}Environment=HTML_STATIC=../html/\n"
-SERVICE_CONTENT=$"${SERVICE_CONTENT}Environment=PORT=8080\n"
-SERVICE_CONTENT=$"${SERVICE_CONTENT}Environment=NODE_ENV=production\n"
-SERVICE_CONTENT=$"${SERVICE_CONTENT}EnvironmentFile=$SERVER_ENV_FILE_PATH\n"
+SERVICE_CONTENT=$"${SERVICE_CONTENT}Environment=PORT=8081\n"
+SERVICE_CONTENT=$"${SERVICE_CONTENT}Environment=NODE_ENV=beta\n"
+SERVICE_CONTENT=$"${SERVICE_CONTENT}EnvironmentFile=$SERVER_ENV_FILE_PATH_BETA\n"
 SERVICE_CONTENT=$"${SERVICE_CONTENT}Restart=always\n"
-SERVICE_CONTENT=$"${SERVICE_CONTENT}User=$SERVER_SERVICE_USER\n"
+SERVICE_CONTENT=$"${SERVICE_CONTENT}User=$SERVER_SERVICE_USER_BETA\n"
 SERVICE_CONTENT=$"${SERVICE_CONTENT}[Install]\n"
 SERVICE_CONTENT=$"${SERVICE_CONTENT}WantedBy=multi-user.target"
  
@@ -40,8 +40,8 @@ chmod +x build/group-car.js
 rm -r node_modules
  
 # Copy files to server to the correct path
-rsync --recursive --times --compress --delete --quiet ./ $SERVER_USER@$SERVER_IP:$SERVER_PATH
+rsync --recursive --times --compress --delete --quiet ./ $SERVER_USER@$SERVER_IP:$SERVER_PATH_BETA
 
  
 # Execute remote install script on server
-ssh $SERVER_USER@$SERVER_IP $SERVER_PATH/scripts/remote_install.sh $SERVER_PATH
+ssh $SERVER_USER@$SERVER_IP $SERVER_PATH_BETA/scripts/remote_install_beta.sh $SERVER_PATH_BETA
