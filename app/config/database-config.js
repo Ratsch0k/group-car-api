c3248fff224edb4f763b9e2cff921e1c532ef08f
Simon Kurz <simonlukas.98@gmail.com>
2020-04-15 23:06:59 +0200

    Changed to not use beta as NODE_EN (beta server should use production environmnent)

--- app/config/database-config.js
+++ app/config/database-config.js
@@ -6,13 +6,6 @@ module.exports = {
     host: process.env.DB_HOSTNAME,
     dialect: 'postgres',
   },
-  beta: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    dialect: 'postgres',
-  },
   test: {
     username: 'group-car-api-test',
     password: null,
