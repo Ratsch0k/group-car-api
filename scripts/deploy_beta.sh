20e44309d882170efaedc5db117be588bc754815
Simon Kurz <simonlukas.98@gmail.com>
2020-04-15 22:49:29 +0200

    Changed service file name to appropriate name

--- scripts/deploy_beta.sh
+++ scripts/deploy_beta.sh
@@ -30,7 +30,7 @@ SERVICE_CONTENT=$"${SERVICE_CONTENT}[Install]\n"
 SERVICE_CONTENT=$"${SERVICE_CONTENT}WantedBy=multi-user.target"
  
 touch server.service
-echo -e "$SERVICE_CONTENT" > group-car.service
+echo -e "$SERVICE_CONTENT" > group-car-beta.service
  
 # Create new folder to use as repository, copy data and remove unnecessary files
 chmod +x scripts/remote_install_beta.sh
