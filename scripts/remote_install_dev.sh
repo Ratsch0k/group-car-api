a976e653ada87fd548f7474afb0bbb66abcbc6c3
Simon Kurz <simonlukas.98@gmail.com>
2020-04-17 23:47:55 +0200

    Removed debugging lines

--- scripts/remote_install_dev.sh
+++ scripts/remote_install_dev.sh
@@ -9,11 +9,6 @@ npm install
 # Get secrets from environment file
 source $2
 export DB_USERNAME DB_HOSTNAME DB_PASSWORD DB_NAME
-cat $2
-echo $DB_USERNAME
-echo $DB_HOSTNAME
-echo $DB_PASSWORD
-echo $DB_NAME
 
 # Migrate database changes
 npm run devMigrate
