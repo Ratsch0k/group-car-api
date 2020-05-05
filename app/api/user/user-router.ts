c7eea1be92ba688b2e5506f339b01b42b0d4e7f1
Simon Kurz <simonlukas.98@gmail.com>
2020-05-05 14:54:43 +0200

    Added new route to get profile picture of user

--- app/api/user/user-router.ts
+++ app/api/user/user-router.ts
@@ -2,6 +2,12 @@ import express from 'express';
 import User from '@app/user/user';
 import ModelToDtoConverter from '@app/util/model-to-dto-converter';
 import UserDto from '../../user/user-dto';
+import {ProfilePic} from '@app/user';
+import NotFoundError from '@app/errors/not-found-error';
+import debug from 'debug';
+
+const log = debug('group-car:user-router');
+const error = debug('group-car:user-router:error');
 
 const router: express.Router = express.Router();
 
@@ -17,9 +23,31 @@ const userRouter: express.RequestHandler = (req, res) => {
   });
 };
 
+const userProfilePicRouter: express.RequestHandler = (req, res, next) => {
+  const userId = req.params.userId;
+  log('User id: %s', userId);
+
+  ProfilePic.findOne({
+    where: {
+      userId,
+    },
+  }).then((pic) => {
+    if (pic !== null) {
+      log('Profile picture found');
+      console.log(pic.data);
+      res.type('image/jpeg');
+      res.send(pic.data);
+    } else {
+      error('Profile picture doesn\'t exist');
+      next(new NotFoundError());
+    }
+  });
+};
+
 /**
  * Add the {@link userRouter} to the get route
  */
 router.get('/', userRouter);
+router.get('/:userId/profile-pic', userProfilePicRouter);
 
 export default router;
