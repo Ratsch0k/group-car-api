aae6eb4d361914f34bb9905c9f0ff0cda1a9995f
Simon Kurz <simonlukas.98@gmail.com>
2020-05-05 14:54:05 +0200

    Added foreign key of user id to profile pic

--- app/user/profile-pic.ts
+++ app/user/profile-pic.ts
@@ -13,6 +13,11 @@ class ProfilePic extends Model {
    * Binary data of the profile picture.
    */
   public data!: string;
+
+  /**
+   * Primary key of the user to which the profile pictures belongs.
+   */
+  public readonly userId!: number;
 }
 
 ProfilePic.init({
