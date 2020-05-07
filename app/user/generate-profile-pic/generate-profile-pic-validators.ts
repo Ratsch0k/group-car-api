ee851ef1097b883f4c6f8ad6f6e39b3eae951106
Simon Kurz <simonlukas.98@gmail.com>
2020-05-07 18:17:14 +0200

    Added validation for offset param for generate profile pic route

--- app/user/generate-profile-pic/generate-profile-pic-validators.ts
+++ app/user/generate-profile-pic/generate-profile-pic-validators.ts
@@ -1,8 +1,14 @@
-import {check} from 'express-validator';
+import {query, oneOf} from 'express-validator';
 
 export default {
   validator: [
-    check('username').notEmpty()
+    query('username').notEmpty()
         .withMessage('Username should not be empty').escape().trim(),
+    oneOf([
+      query('offset').optional(),
+      query('offset').isNumeric({no_symbols: true})
+          .withMessage('Offset has to be either undefined or ' +
+            'a string which represents an integer').toInt(10),
+    ]),
   ],
 };
