7507e1dc25d0d961e0ecf2ee1d1472409cdd3019
Simon Kurz <simonlukas.98@gmail.com>
2020-05-12 18:03:23 +0200

    Fix: remote install scripts not able to use globally installed yarn

--- scripts/remote_install_dev.sh
+++ scripts/remote_install_dev.sh
@@ -4,8 +4,8 @@ set -e
 cd $1
 
 # Install dependencies
-npm_config_build_from_source=true yarn add canvas
-yarn install
+npm_config_build_from_source=true npx yarn add canvas
+npx yarn install
 
 # Change owner of node_modules to deploy. Necessary for deploy user to update server
 sudo chown -R deploy node_modules
@@ -15,7 +15,7 @@ source $2 >/dev/null
 export DB_USERNAME DB_HOSTNAME DB_PASSWORD DB_NAME >/dev/null
 
 # Migrate database changes
-yarn migrate:dev
+npx yarn migrate:dev
 
 # Restart service
 sudo systemctl daemon-reload
