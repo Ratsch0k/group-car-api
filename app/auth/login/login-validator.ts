b83d02e7747ddd705bd32a29e212d4b48f370066
Simon Kurz <simon.kurz@rocketmail.com>
2020-06-02 15:35:29 +0200

    Refactored login validator structure

--- app/auth/login/login-validator.ts
+++ app/auth/login/login-validator.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import debug from 'debug';
-import loginValidators from './login-validators';
 import loginController from './login-controller';
-import {validationResult} from 'express-validator';
+import {validationResult, check} from 'express-validator';
 import {InvalidRequestError} from '@app/errors';
 
 const log = debug('group-car:login:log');
@@ -13,7 +12,10 @@ const router: express.Router = express.Router();
  * @param req Http request
  * @param res Http response
  */
-export const loginValidator: express.RequestHandler = (req, res, next) => {
+export const loginValidationHandler: express.RequestHandler = (
+    req,
+    res,
+    next) => {
   log('IP %s requested login for user "%s"', req.ip, req.body.username);
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -23,9 +25,21 @@ export const loginValidator: express.RequestHandler = (req, res, next) => {
   }
 };
 
+export const loginValidator = {
+  validator: [
+    check('username').notEmpty().escape().trim(),
+    check('password').notEmpty(),
+  ],
+};
+
 /**
- * Add the {@link loginValidator} to the router
+ * Add the {@link loginValidationHandler} to the router
  */
-router.put('/', loginValidators.validator, loginValidator, loginController);
+router.put(
+    '/',
+    loginValidator.validator,
+    loginValidationHandler,
+    loginController,
+);
 
 export default router;
--- app/auth/login/login-validators.ts
+++
@@ -1,8 +0,0 @@
-import {check} from 'express-validator';
-
-export default {
-  validator: [
-    check('username').notEmpty().escape().trim(),
-    check('password').notEmpty(),
-  ],
-};
