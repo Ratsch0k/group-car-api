286c4e775e8bbb36bb07ff92de2eea0d1cdbbf0d
Simon Kurz <simon.kurz@rocketmail.com>
2020-06-03 14:18:49 +0200

    Added membership model

---
+++ app/models/membership/membership.ts
@@ -0,0 +1,49 @@
+import {Model, DataTypes} from 'sequelize';
+import {default as sequelize} from '@db';
+
+/**
+ * Model class for memberships.\
+ * If a user has joined a group,
+ * this relation is stored in this table
+ * as a membership.\
+ * A membership also defines if the user
+ * is an admin of the group.
+ */
+class Membership extends Model {
+  /**
+   * Id the the user.
+   */
+  public readonly userId!: number;
+
+  /**
+   * Id of the group.
+   */
+   public readonly groupId!: number;
+
+   /**
+    * Whether or not the user is an admin of the group.
+    */
+  public isAdmin!: boolean;
+
+  /**
+   * Date when the membership was created.
+   */
+  public readonly createdAt!: Date;
+
+  /**
+   * Date when the membership was last updated.
+   */
+  public readonly updatedAt!: Date;
+};
+
+Membership.init(
+    {
+      isAdmin: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: 'membership',
+    },
+);
+
+export default Membership;
