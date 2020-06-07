a80141eacb281e6ca87c69513b6a6e09c1195db8
Simon Kurz <simon.kurz@rocketmail.com>
2020-06-07 20:36:16 +0200

    Split upateGroupController into two request handlers

--- app/routes/api/group/update/update-group-controller.ts
+++ app/routes/api/group/update/update-group-controller.ts
@@ -1,7 +1,75 @@
+import {Membership, Group} from '@app/models';
+import {
+  BadRequestError,
+  NotMemberOfGroupError,
+  NotAdminOfGroupError,
+  GroupNotFoundError,
+} from '@app/errors';
+import {Router} from 'express';
+
 type RequestHandler = import('express').RequestHandler;
 
-const updateGroupController: RequestHandler= (req, res) => {
-  res.send(req.params.groupId);
+export const updateGroupRequestChecker: RequestHandler= (req, res, next) => {
+  const userId = req.user?.id;
+  const groupId = parseInt(req.params.groupId, 10);
+
+  if (userId && groupId) {
+    /*
+     * Check if user which requested the action is an admin
+     * of the group the users intends to update
+     */
+    Membership.findOne({
+      where: {
+        userId,
+        groupId,
+      },
+    }).then((membership: Membership | null) => {
+      if (membership === null) {
+        next(new NotMemberOfGroupError());
+      } else if (!membership.isAdmin) {
+        next(new NotAdminOfGroupError());
+      } else {
+        // Check if the group exists
+        Group.findByPk(req.params.groupId)
+            .then((group: Group | null) => {
+              if (group == null) {
+                next(new GroupNotFoundError(groupId));
+              } else {
+                next();
+              }
+            }).catch((error) => {
+              next(error);
+            });
+      }
+    });
+  } else {
+    throw new BadRequestError();
+  }
 };
+
+export const updateGroupRequestHandler: RequestHandler = (req, res, next) => {
+  Group.update(
+      {
+        name: req.body.name,
+        description: req.body.description,
+      },
+      {
+        where: {
+          id: req.params.groupId,
+        },
+        returning: true,
+      })
+      .then((value: [number, Group[]]) => {
+        res.send(value[1][0]);
+      }).catch((error) => {
+        next(error);
+      });
+};
+
+const updateGroupController = Router({mergeParams: true}).use(
+    '/',
+    updateGroupRequestChecker,
+    updateGroupRequestHandler,
+);
 
 export default updateGroupController;
