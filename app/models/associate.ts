ed5f0f2a25519022be27ed587efa7cd342457482
Simon Kurz <simon.kurz@rocketmail.com>
2020-06-19 00:50:03 +0200

    Add associations for invite model

--- app/models/associate.ts
+++ app/models/associate.ts
@@ -2,9 +2,10 @@ import {Group} from './group';
 import {User} from './user';
 import {ProfilePic} from './profilePicture';
 import {Membership} from './membership';
+import {Invite} from './invite';
 
 /*
- * Add n:m association between group and user
+ * Add n:m association "Membership" between group and user
  */
 Group.belongsToMany(User, {through: Membership});
 User.belongsToMany(Group, {through: Membership});
@@ -20,3 +21,16 @@ Group.belongsTo(User, {
 });
 
 ProfilePic.belongsTo(User);
+
+/*
+ * Create the n:m association "Invite" between User and Group
+ */
+Group.belongsToMany(User, {through: Invite});
+User.belongsToMany(Group, {through: Invite});
+Invite.belongsTo(Group, {as: 'Group', foreignKey: 'groupId'});
+Invite.belongsTo(User, {as: 'User', foreignKey: 'userId'});
+
+/**
+ * Add the invitedBy association between Invite and User
+ */
+Invite.belongsTo(User, {as: 'InviteSender', foreignKey: 'invitedBy'});
