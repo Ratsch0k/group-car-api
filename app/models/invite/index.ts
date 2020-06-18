2c16f798bbc948e3f1e84c19c06c229b12eb4330
Simon Kurz <simon.kurz@rocketmail.com>
2020-06-19 00:49:52 +0200

    Add invite model

---
+++ app/models/invite/index.ts
@@ -0,0 +1,1 @@
+export {default as Invite} from './invite';
---
+++ app/models/invite/invite.ts
@@ -0,0 +1,40 @@
+import {Model} from 'sequelize';
+import sequelize from '@db';
+
+/**
+ * Model for invites.
+ *
+ * With an invite a user can join a group.
+ */
+class Invite extends Model {
+  /**
+   * The id of the user.
+   */
+  public userId!: number;
+
+  /**
+   * The id of the group.
+   */
+  public groupId!: number;
+
+  /**
+   * The id of the user which send this invite.
+   */
+  public invitedBy!: number;
+
+  /**
+   * The date and time when the invite was created.
+   */
+  public createdAt!: Date;
+}
+
+Invite.init(
+    {},
+    {
+      sequelize,
+      modelName: 'invite',
+      updatedAt: false, // Only use createdAt
+    },
+);
+
+export default Invite;
