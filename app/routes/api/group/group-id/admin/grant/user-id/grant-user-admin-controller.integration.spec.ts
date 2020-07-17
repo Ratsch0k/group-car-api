9da7e94ea78bb8d3f392ef75e110a103587af851
Simon Kurz <simon.kurz@rocketmail.com>
2020-07-17 12:01:04 +0200

    Added rest of integration tests for new endpoint

--- app/routes/api/group/group-id/admin/grant/user-id/grant-user-admin-controller.integration.spec.ts
+++ app/routes/api/group/group-id/admin/grant/user-id/grant-user-admin-controller.integration.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {syncPromise} from '../../../../../../../db';
 import {TestUtils} from '../../../../../../../util/test-utils.spec';
@@ -118,12 +119,73 @@ describe('/api/group/:groupId/admin/grant/:userId', function() {
     await agent.put(`/api/group/${group.id}/admin/grant/${user.id}`)
         .set(csrfHeaderName, csrf)
         .send()
-        .expect(401)
+        .expect(400)
         .then((res) => {
           expect(res.body.message).to
               .equal(new CannotChangeOwnerMembershipError().message);
         });
   });
 
-  it('changes admin value to true');
+  it('changes admin value to true', async function() {
+    const group = await agent.post('/api/group')
+        .set(csrfHeaderName, csrf)
+        .send({
+          name: 'NAME',
+          description: 'DESC',
+        })
+        .expect(201)
+        .then((res) => res.body);
+
+    // Create new user
+    const notAdminUser = await agent.post('/auth/sign-up')
+        .set(csrfHeaderName, csrf)
+        .send({
+          username: 'NOT_ADMIN_USER',
+          password: 'NOT_ADMIN_USER',
+          email: 'NOT_ADMIN@mail.com',
+        }).expect(201)
+        .then((res) => res.body);
+
+    // Create admin for group
+    const adminUser = await agent.post('/auth/sign-up')
+        .set(csrfHeaderName, csrf)
+        .send({
+          username: 'ADMIN',
+          password: 'ADMIN_PASSWORD',
+          email: 'ADMIN@mail.com',
+        })
+        .expect(201)
+        .then((res) => res.body);
+
+    // Shortcut by creating membership for new not admin user
+    await Membership.create({
+      groupId: group.id,
+      userId: notAdminUser.id,
+      isAdmin: false,
+    });
+
+    // Shortcut by creating membership for new admin user
+    await Membership.create({
+      groupId: group.id,
+      userId: adminUser.id,
+      isAdmin: true,
+    });
+
+    // Send request while being logged in as admin
+    await agent.put(`/api/group/${group.id}/admin/grant/${notAdminUser.id}`)
+        .set(csrfHeaderName, csrf)
+        .send()
+        .expect(204);
+
+    // Check if not admin user is now an admin
+    const notAdminMembership = await Membership.findOne({
+      where: {
+        groupId: group.id,
+        userId: notAdminUser.id,
+      },
+    });
+
+    expect(notAdminMembership).to.be.not.null;
+    expect(notAdminMembership!.isAdmin).to.be.true;
+  });
 });
