43ee3b1ef1de691893f28ff475131646630bb92a
Simon Kurz <simon.kurz@rocketmail.com>
2020-07-26 22:20:51 +0200

    Added rest of unit tests

--- app/models/group/group-repository.unit.spec.ts
+++ app/models/group/group-repository.unit.spec.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {expect} from 'chai';
-import {GroupRepository} from '../../models';
+import {GroupRepository, User} from '../../models';
 import Group from './group';
 import sinon, {assert, match} from 'sinon';
 import {GroupNotFoundError} from '../../errors';
 import {MembershipRepository} from '../membership';
+import {Op} from 'sequelize';
 
 describe('GroupRepository', function() {
   afterEach(function() {
@@ -130,6 +131,65 @@ describe('GroupRepository', function() {
           .to.eventually.be.rejectedWith(GroupNotFoundError);
 
       assert.calledOnceWithExactly(findByPkStub, groupId, match.any);
+    });
+  });
+
+  describe('findAllWithIds', function() {
+    let groupFindAll: sinon.SinonStub<any, any>;
+
+    beforeEach(function() {
+      groupFindAll = sinon.stub(Group, 'findAll');
+    });
+
+    it('throws TypeError if any id of the specified ids ' +
+    'list is not a number', async function() {
+      const ids = [1, 2, 3, 4, 'test', 6, 7, 8];
+
+      await expect(GroupRepository.findAllWithIds(ids as any))
+          .to.be.eventually.rejectedWith(TypeError);
+
+      assert.notCalled(groupFindAll);
+    });
+
+    it('returns the list of groups with the specified ids', async function() {
+      const ids = [1, 2];
+
+      const groups = [
+        {
+          id: 1,
+          name: 'group_1',
+        },
+        {
+          id: 2,
+          name: 'group_2',
+        },
+      ];
+
+      groupFindAll.resolves(groups as any);
+
+      const options = {
+        transaction: {},
+      };
+
+      await expect(GroupRepository.findAllWithIds(ids as any, options as any))
+          .to.be.eventually.fulfilled;
+
+      const expected = [{id: 1}, {id: 2}];
+
+      assert.calledOnceWithExactly(
+          groupFindAll,
+          match({
+            where: {
+              [Op.or]: expected,
+            },
+            include: [{
+              model: User,
+              as: 'Owner',
+              attributes: User.simpleAttributes,
+            }],
+            ...options,
+          }),
+      );
     });
   });
 });
