9a12b850e40c7096247518088194229f17a78e14
Simon Kurz <simon.kurz@rocketmail.com>
2020-11-01 23:13:09 +0100

    Added isMember method to MembershipService

--- app/models/membership/membership-service.ts
+++ app/models/membership/membership-service.ts
@@ -197,4 +197,26 @@ export class MembershipService {
         },
     );
   }
+
+  /**
+   * Checks if the current user is a member
+   * of the specified group.
+   * @param currentUser - The currently logged in user
+   * @param groupId     - The if of the group.
+   */
+  public static async isMember(
+      currentUser: Express.User,
+      groupId: number,
+  ): Promise<boolean> {
+    try {
+      await MembershipRepository.findById({groupId, userId: currentUser.id});
+      return true;
+    } catch (e) {
+      if (e instanceof MembershipNotFoundError) {
+        return false;
+      } else {
+        throw e;
+      }
+    }
+  }
 }
