d57b22da81364fc634e18e779bdaa3aef4d60be9
Simon Kurz <simon.kurz@rocketmail.com>
2020-11-02 18:58:20 +0100

    Added comment to driveCarController

--- app/routes/api/group/group-id/car/car-id/drive/drive-car-controller.ts
+++ app/routes/api/group/group-id/car/car-id/drive/drive-car-controller.ts
@@ -2,6 +2,12 @@ import {BadRequestError} from '@app/errors';
 import {CarService} from '@app/models';
 import {RequestHandler} from 'express';
 
+/**
+ * Controller for a user to request to drive the specified car.
+ * @param req   - Request
+ * @param res   - Response
+ * @param next  - Next
+ */
 export const driveCarController: RequestHandler = async (req, res, next) => {
   const user = req.user;
   const groupId = parseInt(req.params.groupId, 10);
