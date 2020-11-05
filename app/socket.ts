e4a2db4224fa28bc9838dc8f89b93ac38489adf1
Simon Kurz <simon.kurz@rocketmail.com>
2020-11-05 14:00:23 +0100

    Added comment

--- app/socket.ts
+++ app/socket.ts
@@ -2,6 +2,10 @@ import http from 'http';
 import socketIo, {Server} from 'socket.io';
 import {GroupNotificationService} from '@models';
 
+/**
+ * Initialize everything socket.io related.
+ * @param server - The used http server
+ */
 const initSocketIoServer = (server: http.Server): Server => {
   const io = socketIo(server, {serveClient: false, path: '/socket'});
 
