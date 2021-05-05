02be901760aafebe2197ba6523ae735ce8ea26cd
Simon Kurz <simon.kurz@rocketmail.com>
2021-05-05 19:34:37 +0200

    Store token as var before login

--- scripts/update_server.sh
+++ scripts/update_server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -ev
 
+PASSWORD=$(<$2)
+
 echo "Login to docker"
-docker login https://docker.pkg.github.com -u Ratsch0k -p $(cat $2)
+docker login https://docker.pkg.github.com -u Ratsch0k -p $PASSWORD
 
 echo "Update server"
 sudo /usr/local/bin/docker-compose -f /tmp/docker-compose.yml -f /tmp/config.yml up -d --build --no-deps $1
