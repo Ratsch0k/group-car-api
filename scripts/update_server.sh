d4d6366ad152558c23d85f8c87b980b3ab447fbd
Simon Kurz <simon.kurz@rocketmail.com>
2021-05-05 23:48:12 +0200

    Trying to fix permission problems

--- scripts/update_server.sh
+++ scripts/update_server.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -ev
 
-# Set environment variables to use rootles docker instance
-export PATH=/usr/bin:$PATH
-export DOCKER_HOST=unix:///run/user/1001/docker.sock
-
 
 echo "Login to docker"
 cat $2 | docker login https://docker.pkg.github.com -u Ratsch0k --password-stdin
