a46ab6d8fffe1eb552fe225fc16747c6a3e6f8f7
Simon Kurz <simon.kurz@rocketmail.com>
2021-05-05 21:17:30 +0200

    Fixed typo

--- scripts/update_server.sh
+++ scripts/update_server.sh
@@ -7,7 +7,7 @@ export DOCKER_HOST=unix:///run/user/1001/docker.sock
 
 
 echo "Login to docker"
-cat $2 | docker login https://docker.pkg.github.com -u Ratsch0k --pasword-stdin
+cat $2 | docker login https://docker.pkg.github.com -u Ratsch0k --password-stdin
 
 echo "Update server"
 sudo /usr/local/bin/docker-compose -f /tmp/docker-compose.yml -f /tmp/config.yml up -d --build --no-deps $1
