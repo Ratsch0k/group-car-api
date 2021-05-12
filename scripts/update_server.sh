6ad73eed6e1107487d2db8bd782e51bc195d861c
Simon Kurz <simon.kurz@rocketmail.com>
2021-05-13 00:57:30 +0200

    Added latest tag when pulling image

--- scripts/update_server.sh
+++ scripts/update_server.sh
@@ -6,7 +6,7 @@ echo "Login to docker"
 < $2 docker login https://docker.pkg.github.com -u Ratsch0k --password-stdin
 
 echo "Pull newest image"
-sudo /usr/bin/docker pull docker.pkg.github.com/ratsch0k/group-car-api/$3
+sudo /usr/bin/docker pull docker.pkg.github.com/ratsch0k/group-car-api/$3:latest
 
 echo "Update server"
 {
